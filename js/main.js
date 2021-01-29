7b39121693b867f5a85cd8bfef546f220d117a96
Klajdi Hoxha <klainti95@gmail.com>
2021-01-29 15:20:33 +0100

    finished calculator functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-const keys = document.querySelector(".calculator__keys");
-const display = document.querySelector(".calculator__display");
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = calculator.querySelector(".calculator__display");
+const clear = calculator.querySelector(".clear");
 
 keys.addEventListener("click", event => {
     if(!event.target.closest("button")) return;
@@ -7,13 +9,52 @@ keys.addEventListener("click", event => {
     const key = event.target;
     const keyValue = key.textContent;
     const displayValue = display.textContent;
+    const { type } = key.dataset;
+    const { previousKeyType } = calculator.dataset;
     
-    if(displayValue === "0"){
-        display.textContent = keyValue;
-    }else{
-        display.textContent = displayValue + keyValue;
+    // is this a number key
+    if(type === "number"){
+        if(displayValue === "0" || previousKeyType === "operator"){
+            display.textContent = keyValue;
+        }else{
+            display.textContent = displayValue + keyValue;
+        }
     }
-    
-    // console.log(displayValue);
 
+
+    //is this an operator key
+    if(type === "operator"){
+        const operatorKeys = keys.querySelectorAll("[data-type='operator']");
+        operatorKeys.forEach(el => { el.dataset.state = "" });
+        key.dataset.state = "selected";
+
+        calculator.dataset.firstNumber = displayValue;
+        calculator.dataset.operator = key.dataset.key;
+    }
+
+    if(type === "equal"){
+        const firstNumber = calculator.dataset.firstNumber;
+        const operator  = calculator.dataset.operator;
+        const secondNumber = displayValue;
+        display.textContent = calculate(firstNumber, operator, secondNumber);
+    }
+
+    if(type === "clear"){
+        display.textContent = "0";
+        delete calculator.dataset.firstNumber;
+        delete calculator.dataset.operator;
+    }
+
+    calculator.dataset.previousKeyType = type;
 });
+
+
+function calculate(firstNumber, operator, secondNumber){
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+    
+    if(operator === "plus") return firstNumber + secondNumber;
+    if(operator === "minus") return firstNumber - secondNumber;
+    if(operator === "times") return firstNumber * secondNumber;
+    if(operator === "divide") return firstNumber / secondNumber;
+}
